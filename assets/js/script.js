2188b414a497b229cb9f3b43ae51cc6ff94fda55
Royce <royceatkins93@gmail.com>
2024-04-16 15:55:37 -0400

    added spell list function and form submit handler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,54 @@
 //global variables
-const searchBtn = document.querySelector("#search");
+const searchInput = document.querySelector("#search-name");
+const searchInputBtn = document.querySelector("#search");
+const searchBtn = document.querySelector("#search-button");
+
+let allSpells = JSON.parse(localStorage.getItem('spellList')) || [];
+let spells = JSON.parse(localStorage.getItem('spellData'));
+let classData = JSON.parse(localStorage.getItem('classes'));
 
 
-let spellList = JSON.parse(localStorage.getItem('spells')) || [];
-
+//form submit handler
+const formSubmitHandler = function
 //function to get Open5e.com for spell list
-const spellSearch = function (spellList) {
+const spellListSearch = function () {
     const queryURL = `https://api.open5e.com/v1/spelllist/`;
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.ok) {
+                //clear the search bar -- to do
+                //run spell list array handler  --- to do
+                localStorage.setItem(`spellList`, JSON.stringify(allSpells))
+
+                // displaySpell(data); -- to do
+                // spellListSearch(data); -- to do
+                return response.json();
+            }
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+};
+
+
+//function to get individual spell
+const spellSearch = function () {
+    const queryURL = `https://api.open5e.com/v1/spells/?slug__in=${searchInput}&slug__iexact=&slug=&name__iexact=&name=&spell_level=&spell_level__range=&spell_level__gt=&spell_level__gte=&spell_level__lt=&spell_level__lte=&target_range_sort=&target_range_sort__range=&target_range_sort__gt=&target_range_sort__gte=&target_range_sort__lt=&target_range_sort__lte=&school__iexact=&school=&school__in=&duration__iexact=&duration=&duration__in=&requires_concentration=unknown&requires_verbal_components=unknown&requires_somatic_components=unknown&requires_material_components=unknown&casting_time__iexact=&casting_time=&casting_time__in=&dnd_class__iexact=&dnd_class=&dnd_class__in=&dnd_class__icontains=&document__slug__iexact=&document__slug=&document__slug__in=&document__slug__not_in=&level_int=&concentration=&components=&spell_lists_not=`;
+
     fetch(queryURL)
     .then(function (response) {
         if (response.ok) {
-            //clear the search bar -- to do
-            //run spell list array handler  --- to do
-            localStorage.setItem(`spellList`, JSON.stringify(spellHistory))
-            console.log('spellList')
+        //past spell array handler
+        
+            localStorage.setItem('spellData', JSON.stringify())
         }
-    })
-    
+    }
+)
 }
-
-//function to get individual spell
-
 //function to get 5e-bits for class info
 
 //event listener for search button
+searchBtn.addEventListener('click', spellListSearch);
 
 //modal for possible spell candidates drop down or list
 
