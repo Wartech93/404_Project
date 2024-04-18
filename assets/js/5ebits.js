ac424bf1089f24a0e48f4a04406de69c0e83ee9a
Royce <royceatkins93@gmail.com>
2024-04-18 10:14:56 -0400

    completed and functioning First API fetch and append to dom

---
+++ assets/js/5ebits.js
@@ -0,0 +1,19 @@
+// //global variables
+// let classInfo = JSON.parse(localStorage.getItem(`spellArray`)) || [];
+
+
+// //get 5ebits api response
+// const classInfoSearch = function (data {
+//     const queryURL = `https://www.dnd5eapi.co/api/classes/${data}/spellcasting`
+//     fetch(queryURL)
+//     .then(function(response) {
+//         if (response.ok)
+//     } 
+// return response.json();)
+// )
+// })
+//function to create array for api response
+
+//event listener for each class button
+
+//append class info to the right of the page for each button (column-right)
